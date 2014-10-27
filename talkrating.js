68e9e18b967256ecb6bc3497515e032cc0c0d33d
r3dDoX <patrick@walther-online.ch>
2014-10-27 20:35:45 +0100

    stage 1

--- talkrating.js
+++ talkrating.js
@@ -1,23 +1,13 @@
+Talks = new Mongo.Collection('talks');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+    Template.talks.helpers({
+        talks: function () {
+            return Talks.find().fetch();
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
